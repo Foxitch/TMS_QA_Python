bf7b023b6c6d3081522bd3677b0bd2dc29870eb0
lazko.maksim <maks99965@gmail.com>
2022-04-19 19:49:05 +0300

    translated on English

--- lesson_3/Lazko, lesson3.py
+++ lesson_3/Lazko, lesson3.py
@@ -20,20 +20,19 @@
 age: int = int(input('Введите возраст:'))
 name: str = input('Введите имя:').capitalize().strip()
 
-# Ян вышел из чата...
 if len(name) < 3:
-    a = 'Имя не может содержать менее 3-х символов'
+    a = 'Name cannot be less than 3 symbols'
 elif age <= 0:
-    a = 'Возраст не может быть отрицательным или равен 0'
+    a = 'Name cannot be negative or 0'
 elif 0 < age < 14:
-    a = 'Минимальный возраст для входа в систему - 14 лет'
+    a = 'Min age to login is 14'
 elif 16 <= age <= 17:
-    a = f'Приветсвую вас, {name}. Ваш возраст {age}. Не забудьте получить Ваш первый паспорт'
+    a = f'Hello, {name}. Your age is {age}. Do not forget to get your first passport'
 elif 25 <= age <= 26:
-    a = f'Приветсвую вас, {name}. Ваш возраст {age}. Не забудьте заменить паспорт'
+    a = f'Hello, {name}. Your age is {age}. Do not forget to change your passport'
 elif 45 <= age <= 46:
-    a = f'Приветсвую вас, {name}. Ваш возраст {age}. Пожалуйста, не забудьте обновить паспорт'
+    a = f'Hello, {name}. Your age is {age}. Do not forget to update your passport'
 else:
-    a = f'Приветствую вас, {name}. Ваш возраст {age}'
+    a = f'Hello, {name}. Your age is {age}'
 
 print(a)
